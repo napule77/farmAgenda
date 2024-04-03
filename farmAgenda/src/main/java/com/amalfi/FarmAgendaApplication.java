4dd5e8489b7040d15411376cf037fb39f4fae8ed
Amalfi <Amalfi@DESKTOP-EF3JD3P.homenet.telecomitalia.it>
2024-04-03 21:48:32 +0200

    Startup Release

---
+++ farmAgenda/src/main/java/com/amalfi/FarmAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.amalfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmAgendaApplication.class, args);
+	}
+
+}
---
+++ farmAgenda/src/test/java/com/amalfi/FarmAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
